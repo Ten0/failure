07bf23596ae41ad6a0c59b1f363de0552f994348
Thomas BESSOU <thomas.bessou@hotmail.fr>
2018-12-27 23:36:18 +0100

    can't work because of https://github.com/rust-lang/rfcs/issues/1053 but would be cool coherent backtrace implementation for Compat<failure::Error>

--- src/lib.rs
+++ src/lib.rs
@@ -23,10 +23,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
 #![deny(warnings)]
-#![cfg_attr(
-    feature = "small-error",
-    feature(extern_types, allocator_api)
-)]
+#![cfg_attr(feature = "small-error", feature(extern_types, allocator_api))]
 
 macro_rules! with_std { ($($i:item)*) => ($(#[cfg(feature = "std")]$i)*) }
 macro_rules! without_std { ($($i:item)*) => ($(#[cfg(not(feature = "std"))]$i)*) }
@@ -63,23 +60,23 @@ extern crate failure_derive;
 pub use failure_derive::*;
 
 with_std! {
-    extern crate core;
+	extern crate core;
 
-    mod sync_failure;
-    pub use sync_failure::SyncFailure;
+	mod sync_failure;
+	pub use sync_failure::SyncFailure;
 
-    mod error;
+	mod error;
 
-    use std::error::Error as StdError;
+	use std::error::Error as StdError;
 
-    pub use error::Error;
+	pub use error::Error;
 
-    /// A common result with an `Error`.
-    pub type Fallible<T> = Result<T, Error>;
+	/// A common result with an `Error`.
+	pub type Fallible<T> = Result<T, Error>;
 
-    mod macros;
-    mod error_message;
-    pub use error_message::err_msg;
+	mod macros;
+	mod error_message;
+	pub use error_message::err_msg;
 }
 
 /// The `Fail` trait.
@@ -109,163 +106,157 @@ with_std! {
 /// `std::error::Error`, and are also `Send`, `Sync`, and `'static`, implement
 /// `Fail` by a blanket impl.
 pub trait Fail: Display + Debug + Send + Sync + 'static {
-    /// Returns a reference to the underlying cause of this failure, if it
-    /// is an error that wraps other errors.
-    ///
-    /// Returns `None` if this failure does not have another error as its
-    /// underlying cause. By default, this returns `None`.
-    ///
-    /// This should **never** return a reference to `self`, but only return
-    /// `Some` when it can return a **different** failure. Users may loop
-    /// over the cause chain, and returning `self` would result in an infinite
-    /// loop.
-    fn cause(&self) -> Option<&Fail> {
-        None
-    }
+	/// Returns a reference to the underlying cause of this failure, if it
+	/// is an error that wraps other errors.
+	///
+	/// Returns `None` if this failure does not have another error as its
+	/// underlying cause. By default, this returns `None`.
+	///
+	/// This should **never** return a reference to `self`, but only return
+	/// `Some` when it can return a **different** failure. Users may loop
+	/// over the cause chain, and returning `self` would result in an infinite
+	/// loop.
+	fn cause(&self) -> Option<&Fail> {
+		None
+	}
 
-    /// Returns a reference to the `Backtrace` carried by this failure, if it
-    /// carries one.
-    ///
-    /// Returns `None` if this failure does not carry a backtrace. By
-    /// default, this returns `None`.
-    fn backtrace(&self) -> Option<&Backtrace> {
-        None
-    }
+	/// Returns a reference to the `Backtrace` carried by this failure, if it
+	/// carries one.
+	///
+	/// Returns `None` if this failure does not carry a backtrace. By
+	/// default, this returns `None`.
+	fn backtrace(&self) -> Option<&Backtrace> {
+		None
+	}
 
-    /// Provides context for this failure.
-    ///
-    /// This can provide additional information about this error, appropriate
-    /// to the semantics of the current layer. That is, if you have a
-    /// lower-level error, such as an IO error, you can provide additional context
-    /// about what that error means in the context of your function. This
-    /// gives users of this function more information about what has gone
-    /// wrong.
-    ///
-    /// This takes any type that implements `Display`, as well as
-    /// `Send`/`Sync`/`'static`. In practice, this means it can take a `String`
-    /// or a string literal, or another failure, or some other custom context-carrying
-    /// type.
-    fn context<D>(self, context: D) -> Context<D>
-    where
-        D: Display + Send + Sync + 'static,
-        Self: Sized,
-    {
-        Context::with_err(context, self)
-    }
+	/// Provides context for this failure.
+	///
+	/// This can provide additional information about this error, appropriate
+	/// to the semantics of the current layer. That is, if you have a
+	/// lower-level error, such as an IO error, you can provide additional context
+	/// about what that error means in the context of your function. This
+	/// gives users of this function more information about what has gone
+	/// wrong.
+	///
+	/// This takes any type that implements `Display`, as well as
+	/// `Send`/`Sync`/`'static`. In practice, this means it can take a `String`
+	/// or a string literal, or another failure, or some other custom context-carrying
+	/// type.
+	fn context<D>(self, context: D) -> Context<D>
+	where
+		D: Display + Send + Sync + 'static,
+		Self: Sized,
+	{
+		Context::with_err(context, self)
+	}
 
-    /// Wraps this failure in a compatibility wrapper that implements
-    /// `std::error::Error`.
-    ///
-    /// This allows failures  to be compatible with older crates that
-    /// expect types that implement the `Error` trait from `std::error`.
-    fn compat(self) -> Compat<Self>
-    where
-        Self: Sized,
-    {
-        Compat { error: self }
-    }
+	/// Wraps this failure in a compatibility wrapper that implements
+	/// `std::error::Error`.
+	///
+	/// This allows failures  to be compatible with older crates that
+	/// expect types that implement the `Error` trait from `std::error`.
+	fn compat(self) -> Compat<Self>
+	where
+		Self: Sized,
+	{
+		Compat { error: self }
+	}
 
-    #[doc(hidden)]
-    #[deprecated(
-        since = "0.1.2",
-        note = "please use the 'iter_chain()' method instead"
-    )]
-    fn causes(&self) -> Causes
-    where
-        Self: Sized,
-    {
-        Causes { fail: Some(self) }
-    }
+	#[doc(hidden)]
+	#[deprecated(since = "0.1.2", note = "please use the 'iter_chain()' method instead")]
+	fn causes(&self) -> Causes
+	where
+		Self: Sized,
+	{
+		Causes { fail: Some(self) }
+	}
 
-    #[doc(hidden)]
-    #[deprecated(
-        since = "0.1.2",
-        note = "please use the 'find_root_cause()' method instead"
-    )]
-    fn root_cause(&self) -> &Fail
-    where
-        Self: Sized,
-    {
-        find_root_cause(self)
-    }
+	#[doc(hidden)]
+	#[deprecated(
+		since = "0.1.2",
+		note = "please use the 'find_root_cause()' method instead"
+	)]
+	fn root_cause(&self) -> &Fail
+	where
+		Self: Sized,
+	{
+		find_root_cause(self)
+	}
 
-    #[doc(hidden)]
-    fn __private_get_type_id__(&self) -> TypeId {
-        TypeId::of::<Self>()
-    }
+	#[doc(hidden)]
+	fn __private_get_type_id__(&self) -> TypeId {
+		TypeId::of::<Self>()
+	}
 }
 
 impl Fail {
-    /// Attempts to downcast this failure to a concrete type by reference.
-    ///
-    /// If the underlying error is not of type `T`, this will return `None`.
-    pub fn downcast_ref<T: Fail>(&self) -> Option<&T> {
-        if self.__private_get_type_id__() == TypeId::of::<T>() {
-            unsafe { Some(&*(self as *const Fail as *const T)) }
-        } else {
-            None
-        }
-    }
+	/// Attempts to downcast this failure to a concrete type by reference.
+	///
+	/// If the underlying error is not of type `T`, this will return `None`.
+	pub fn downcast_ref<T: Fail>(&self) -> Option<&T> {
+		if self.__private_get_type_id__() == TypeId::of::<T>() {
+			unsafe { Some(&*(self as *const Fail as *const T)) }
+		} else {
+			None
+		}
+	}
 
-    /// Attempts to downcast this failure to a concrete type by mutable
-    /// reference.
-    ///
-    /// If the underlying error is not of type `T`, this will return `None`.
-    pub fn downcast_mut<T: Fail>(&mut self) -> Option<&mut T> {
-        if self.__private_get_type_id__() == TypeId::of::<T>() {
-            unsafe { Some(&mut *(self as *mut Fail as *mut T)) }
-        } else {
-            None
-        }
-    }
+	/// Attempts to downcast this failure to a concrete type by mutable
+	/// reference.
+	///
+	/// If the underlying error is not of type `T`, this will return `None`.
+	pub fn downcast_mut<T: Fail>(&mut self) -> Option<&mut T> {
+		if self.__private_get_type_id__() == TypeId::of::<T>() {
+			unsafe { Some(&mut *(self as *mut Fail as *mut T)) }
+		} else {
+			None
+		}
+	}
 
-    /// Returns the "root cause" of this `Fail` - the last value in the
-    /// cause chain which does not return an underlying `cause`.
-    ///
-    /// If this type does not have a cause, `self` is returned, because
-    /// it is its own root cause.
-    ///
-    /// This is equivalent to iterating over `iter_causes()` and taking
-    /// the last item.
-    pub fn find_root_cause(&self) -> &Fail {
-        find_root_cause(self)
-    }
+	/// Returns the "root cause" of this `Fail` - the last value in the
+	/// cause chain which does not return an underlying `cause`.
+	///
+	/// If this type does not have a cause, `self` is returned, because
+	/// it is its own root cause.
+	///
+	/// This is equivalent to iterating over `iter_causes()` and taking
+	/// the last item.
+	pub fn find_root_cause(&self) -> &Fail {
+		find_root_cause(self)
+	}
 
-    /// Returns a iterator over the causes of this `Fail` with the cause
-    /// of this fail as the first item and the `root_cause` as the final item.
-    ///
-    /// Use `iter_chain` to also include the fail itself.
-    pub fn iter_causes(&self) -> Causes {
-        Causes { fail: self.cause() }
-    }
+	/// Returns a iterator over the causes of this `Fail` with the cause
+	/// of this fail as the first item and the `root_cause` as the final item.
+	///
+	/// Use `iter_chain` to also include the fail itself.
+	pub fn iter_causes(&self) -> Causes {
+		Causes { fail: self.cause() }
+	}
 
-    /// Returns a iterator over all fails up the chain from the current
-    /// as the first item up to the `root_cause` as the final item.
-    ///
-    /// This means that the chain also includes the fail itself which
-    /// means that it does *not* start with `cause`.  To skip the outermost
-    /// fail use `iter_causes` instead.
-    pub fn iter_chain(&self) -> Causes {
-        Causes { fail: Some(self) }
-    }
+	/// Returns a iterator over all fails up the chain from the current
+	/// as the first item up to the `root_cause` as the final item.
+	///
+	/// This means that the chain also includes the fail itself which
+	/// means that it does *not* start with `cause`.  To skip the outermost
+	/// fail use `iter_causes` instead.
+	pub fn iter_chain(&self) -> Causes {
+		Causes { fail: Some(self) }
+	}
 
-    /// Deprecated alias to `find_root_cause`.
-    #[deprecated(
-        since = "0.1.2",
-        note = "please use the 'find_root_cause()' method instead"
-    )]
-    pub fn root_cause(&self) -> &Fail {
-        find_root_cause(self)
-    }
+	/// Deprecated alias to `find_root_cause`.
+	#[deprecated(
+		since = "0.1.2",
+		note = "please use the 'find_root_cause()' method instead"
+	)]
+	pub fn root_cause(&self) -> &Fail {
+		find_root_cause(self)
+	}
 
-    /// Deprecated alias to `iter_chain`.
-    #[deprecated(
-        since = "0.1.2",
-        note = "please use the 'iter_chain()' method instead"
-    )]
-    pub fn causes(&self) -> Causes {
-        Causes { fail: Some(self) }
-    }
+	/// Deprecated alias to `iter_chain`.
+	#[deprecated(since = "0.1.2", note = "please use the 'iter_chain()' method instead")]
+	pub fn causes(&self) -> Causes {
+		Causes { fail: Some(self) }
+	}
 }
 
 #[cfg(feature = "std")]
@@ -273,34 +264,34 @@ impl<E: StdError + Send + Sync + 'static> Fail for E {}
 
 #[cfg(feature = "std")]
 impl Fail for Box<Fail> {
-    fn cause(&self) -> Option<&Fail> {
-        (**self).cause()
-    }
+	fn cause(&self) -> Option<&Fail> {
+		(**self).cause()
+	}
 
-    fn backtrace(&self) -> Option<&Backtrace> {
-        (**self).backtrace()
-    }
+	fn backtrace(&self) -> Option<&Backtrace> {
+		(**self).backtrace()
+	}
 }
 
 /// A iterator over the causes of a `Fail`
 pub struct Causes<'f> {
-    fail: Option<&'f Fail>,
+	fail: Option<&'f Fail>,
 }
 
 impl<'f> Iterator for Causes<'f> {
-    type Item = &'f Fail;
-    fn next(&mut self) -> Option<&'f Fail> {
-        self.fail.map(|fail| {
-            self.fail = fail.cause();
-            fail
-        })
-    }
+	type Item = &'f Fail;
+	fn next(&mut self) -> Option<&'f Fail> {
+		self.fail.map(|fail| {
+			self.fail = fail.cause();
+			fail
+		})
+	}
 }
 
 fn find_root_cause(mut fail: &Fail) -> &Fail {
-    while let Some(cause) = fail.cause() {
-        fail = cause;
-    }
+	while let Some(cause) = fail.cause() {
+		fail = cause;
+	}
 
-    fail
+	fail
 }
